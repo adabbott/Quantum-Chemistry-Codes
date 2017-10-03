9d50af513ff8d7f5115e6952d5558826458bde91
adabbott <adabbott@uga.edu>
2017-10-03 15:49:23 -0400

    fixed bugs in Molecule class, all functions now working, all variables now settable

--- Quantum_Chemistry_Module/molecule.py
+++ Quantum_Chemistry_Module/molecule.py
@@ -1,14 +1,11 @@
 # This file defines a molecule class that returns information about the molecule.
-# It also contains member functions set_geometry(str) for setting a geometry from a
-#   string or psi4 geometry, and set_basis(str) for setting up a basis set with psi4.
-
 import psi4
 
 
 class Molecule(object):
     """
     A molecule class that is stores basic parameters, including
-    psi4 geometry, basis set, charge, multiplicty, number of electrons.
+    psi4 geometry, basis set, charge, multiplicty, number of electrons, and electron occupations
 
     """
 
@@ -41,7 +38,7 @@ class Molecule(object):
 
         self.nuclear_repulsion_energy = None
         self.number_of_electrons = None
-        self.dnocc = None
+        self.ndocc = None
         self.nsocc = None
 
         # Sets molecule data
@@ -57,64 +54,70 @@ class Molecule(object):
         if mult is not None:
             self.set_mult(mult)
 
-        if ((mult is not None) and (charge is not None) and (mult is not None)): 
-            self.set_electron_occupations()
+        if ((geometry is not None) and (charge is not None) and (mult is not None)): 
+            self.set_electron_occupations(geometry, charge, mult)
             
-        def set_geometry(self, geometry_string):
-            if isinstance(geom_str, str):
-                self.geometry = psi4.geometry(geometry_string) 
-            else:
-                raise TypeError("Input must be a valid Psi4 geometry")
-            self.geometry_update_geometry()
-            self.nuclear_repulsion_energy = self.geometry.nuclear_repulsion_energy() 
-            
-           
-        def set_basis(self, basis_str):
-            """
-            Set the basis set.
-            Parameters 
-            ----------
-            basis_str : str
-            Psi4 basis set name 
+    def set_geometry(self, geometry_string):
+        """
+        Set the molecules geometry.
+        Parameters 
+        ----------
+        geometry_string : str
+        Geometry string in the form of any valid psi4 geometry 
+        """
+        if isinstance(geometry_string, str):
+            self.geometry = psi4.geometry(geometry_string) 
+        elif isinstance(geometry_string, psi4.core.Molecule):
+            self.geometry = geometry_string
+        else:
+            raise TypeError("Input must be a valid Psi4 geometry string")
+        self.geometry.update_geometry()
+        # grab nuclear repulsion while convenient
+        self.nuclear_repulsion_energy = self.geometry.nuclear_repulsion_energy() 
         
-            Returns
-            -------
-            Psi4 Basis set object
-            """
-            
-            if (self.geometry is None):
-                raise Exeption("Error: Geometry not defined") 
-            else:
-                self.basis = psi4.core.BasisSet.build(self.geometry, target=basis_str)
-
-        def set_charge(self, charge_int):
-            if (self.geometry is None):
-                raise Exeption("Error: Geometry not defined") 
-            if isinstance(charge_int, int):
-                self.geometry.set_molecular_charge(charge_int)
-            else:
-                raise TypeError("Input must be an integer for charge")
-
-        def set_mult(self, mult_int):
-            if (self.geometry is None):
-                raise Exeption("Error: Geometry not defined") 
-            if isinstance(mult_int, int):
-                self.geometry.set_multiplicity(self.mult)
-                self.mult = mult_int 
-            else:
-                raise TypeError("Input must be an integer for charge")
+       
+    def set_basis(self, basis_str):
+        """
+        Set the basis set.
+        Parameters 
+        ----------
+        basis_str : str
+        Psi4 basis set name 
+        """
         
-        def set_electron_occupations(self, geometry = self.geometry, charge = self.charge, mult = self.mult):
-            if (geometry is None):
-                raise Exeption("Error: Geometry not defined") 
-            if (charge is None):
-                raise Exeption("Error: Molecular charge not defined") 
+        if (self.geometry is None):
+            raise Exeption("Error: Geometry not defined") 
+        else:
+            self.basis = psi4.core.BasisSet.build(self.geometry, "BASIS", basis_str, puream=0)
 
-            protons = 0
-            for atom_index in range(geometry.natom()):
-                protons += geometry.charge(i)
+    def set_charge(self, charge_int):
+        if (self.geometry is None):
+            raise Exeption("Error: Geometry not defined") 
+        if isinstance(charge_int, int):
+            self.geometry.set_molecular_charge(charge_int)
+        else:
+            raise TypeError("Input must be an integer for charge")
 
-            self.number_of_electrons = charge + protons
-            self.dnocc = self.number_of_electrons - mult - 1 # Number of doubly occupied orbitals
-            self.nsocc = self.mult - 1                       # Number of singly occupied orbitals
+    def set_mult(self, mult_int):
+        if (self.geometry is None):
+            raise Exeption("Error: Geometry not defined") 
+        if isinstance(mult_int, int):
+            self.geometry.set_multiplicity(self.mult)
+            self.mult = mult_int 
+        else:
+            raise TypeError("Input must be an integer for charge")
+    
+    def set_electron_occupations(self, geometry, charge, mult):
+        if (geometry is None):
+            raise Exeption("Error: Geometry not defined") 
+        if (charge is None):
+            raise Exeption("Error: Molecular charge not defined") 
+
+        protons = 0
+        for atom_index in range(geometry.natom()):
+            protons += geometry.charge(atom_index)
+
+        self.number_of_electrons = charge + protons
+        self.ndocc = self.number_of_electrons - mult - 1 # Number of doubly occupied orbitals
+        self.nsocc = self.mult - 1                       # Number of singly occupied orbitals
 
